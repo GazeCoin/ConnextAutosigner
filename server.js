c9f7ff7d5a1233b6f7a6bb153d79413dbf38f58d
Geoff Lamperd <glamperd@gmail.com>
2019-08-28 15:45:10 +1000

    vary .env location by instance

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('babel-core/register');
-require('dotenv').config();
+require('dotenv').config({ path: './config/' + process.argv[2] + '/.env' });
+console.log('argv ', process.argv[2]);
 
 //TODO - Put this in networking.ts
 global.fetch = require('node-fetch');
